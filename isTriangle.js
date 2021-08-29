d6ac76cdf238c5e6c24517125e41e4dfcb890e16
Aamir Nathani <amirnathani9@gmail.com>
2021-08-30 01:45:07 +0530

    update: triangle condition

--- isTriangle.js
+++ isTriangle.js
@@ -7,17 +7,25 @@ function calculateSumOFAngle(angle1,angle2,angle3){
     return sumOfAngles;
 }
 
-
-
-function isTriangle() {
+function showOutput(){
     const sumOfAngles = calculateSumOFAngle(Number(inputAngle[0].value), Number(inputAngle[1].value), Number(inputAngle[2].value));
-    
-    if(sumOfAngles === 180){
+    if(sumOfAngles === 180 && inputAngle[0].value>0 && inputAngle[1].value>0 && inputAngle[2].value>0){
         outputBox.innerText = "The input angles makes a Triangle";
     }
     else {
         outputBox.innerText = "The input angles don't makes a Triangle"
     }
+}
+
+
+function isTriangle() {
+
+    if(inputAngle[0].value&&inputAngle[1].value&&inputAngle[2].value){
+        showOutput(inputAngle.value)
+    }
+    else{
+        outputBox.innerText="Input all the values"
+    } 
 }
 
 btnOutput.addEventListener("click",isTriangle)
