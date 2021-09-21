513043c1a5b40193f289792c37cb23159b056423
Aamir Nathani <amirnathani9@gmail.com>
2021-09-21 15:38:31 +0530

    updated navbar

--- scripts/hypotenuse.js
+++ scripts/hypotenuse.js
@@ -27,3 +27,19 @@ function calculateHypotenuse() {
 }
 
 btnCalculate.addEventListener("click", calculateHypotenuse)
+
+// for nav toggle
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+function close(){
+    mainMenu.style.top = '-120%';
+}
--- scripts/isTriangle.js
+++ scripts/isTriangle.js
@@ -29,3 +29,19 @@ function isTriangle() {
 }
 
 btnOutput.addEventListener("click",isTriangle)
+
+// for nav toggle
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+function close(){
+    mainMenu.style.top = '-120%';
+}
